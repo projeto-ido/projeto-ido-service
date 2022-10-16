a9e66df6b07bc1c847960333f27511d2e64dd9c9
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-10-16 20:46:28 -0300

    adicionando resposta de dto para usuario

--- src/main/java/school/sptech/ido/application/controller/UsuarioController.java
+++ src/main/java/school/sptech/ido/application/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import school.sptech.ido.resources.repository.entity.UsuarioEntity;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,32 +20,32 @@ public class UsuarioController {
     UsuarioRepository usuarioRepository;
 
     @GetMapping
-    public ResponseEntity<List<UsuarioEntity>> listarUsuarios() {
-        List<UsuarioEntity> usuarios = usuarioRepository.findAll();
+    public ResponseEntity<List<UsuarioDto>> listarUsuarios() {
+        List<UsuarioDto> usuarios = usuarioRepository.findAll().stream().map(UsuarioDto::new).collect(Collectors.toList());
         return usuarios.isEmpty() ? ResponseEntity.status(204).build() : ResponseEntity.ok().body(usuarios);
     }
 
     @PostMapping
-    public ResponseEntity<UsuarioEntity> cadastrarUsuario(@RequestBody @Valid UsuarioCadastroDto usuario) {
-        return ResponseEntity.status(201).body(usuarioRepository.save(new UsuarioEntity(usuario)));
+    public ResponseEntity<UsuarioDto> cadastrarUsuario(@RequestBody @Valid UsuarioCadastroDto usuario) {
+        return ResponseEntity.status(201).body(new UsuarioDto(usuarioRepository.save(new UsuarioEntity(usuario))));
     }
 
     @PutMapping("/{idUsuario}")
-    public ResponseEntity<UsuarioEntity> atualizarUsuario(
+    public ResponseEntity<UsuarioDto> atualizarUsuario(
         @PathVariable Integer idUsuario,
         @RequestBody @Valid UsuarioAtualizadoDto usuarioAtualizadoDto
     ) {
 
         if (usuarioRepository.existsById(idUsuario)){
             UsuarioEntity usuarioAtualizado = new UsuarioEntity(idUsuario, usuarioAtualizadoDto);
-            return ResponseEntity.ok().body(usuarioRepository.save(usuarioAtualizado));
+            return ResponseEntity.ok().body(new UsuarioDto(usuarioRepository.save(usuarioAtualizado)));
         }
 
         return ResponseEntity.notFound().build();
     }
 
     @PatchMapping("/{idUsuario}/nivel/{novoNivel}")
-    public ResponseEntity<UsuarioEntity> atualizarNivel(
+    public ResponseEntity<UsuarioDto> atualizarNivel(
         @PathVariable Integer idUsuario,
         @PathVariable Integer novoNivel
     ) {
@@ -53,14 +54,14 @@ public class UsuarioController {
         if (usuario.isPresent()){
             UsuarioEntity usuarioEncontrado = usuario.get();
             usuarioEncontrado.setNivel(novoNivel);
-            return ResponseEntity.ok().body(usuarioRepository.save(usuarioEncontrado));
+            return ResponseEntity.ok().body(new UsuarioDto(usuarioRepository.save(usuarioEncontrado)));
         }
 
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{idUsuario}")
-    public ResponseEntity<UsuarioEntity> deletarUsuario(@PathVariable Integer idUsuario) {
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Integer idUsuario) {
 
         if (usuarioRepository.existsById(idUsuario)){
             usuarioRepository.deleteById(idUsuario);
@@ -71,7 +72,7 @@ public class UsuarioController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UsuarioEntity> logar(@RequestBody @Valid UsuarioLoginDto usuarioLoginDto) {
+    public ResponseEntity<UsuarioDto> logar(@RequestBody @Valid UsuarioLoginDto usuarioLoginDto) {
         Optional<UsuarioEntity> usuario = usuarioRepository.findByEmailAndSenha(
             usuarioLoginDto.getEmail(),
             usuarioLoginDto.getSenha()
@@ -80,7 +81,7 @@ public class UsuarioController {
         if (usuario.isPresent()){
             UsuarioEntity usuarioLogado = usuario.get();
             usuarioLogado.setAutenticado(true);
-            return ResponseEntity.ok().body(usuarioRepository.save(usuarioLogado));
+            return ResponseEntity.ok().body(new UsuarioDto(usuarioRepository.save(usuarioLogado)));
         }
 
         return ResponseEntity.status(401).build();
