2c8e03356ee35045caa70b378cc4ccb4e0e77b2c
yanhudson <yan.hudson@bandtec.com.br>
2022-12-01 20:43:28 -0300

    ajustando toString de subTarefacoesExportacao

--- src/main/java/school/sptech/ido/application/controller/dto/SubTarefaExportacaoDto.java
+++ src/main/java/school/sptech/ido/application/controller/dto/SubTarefaExportacaoDto.java
@@ -34,6 +34,6 @@ public class SubTarefaExportacaoDto {
     @Override
     public String toString() {
         return String.format(
-                "Subtarefa - %s, %b", this.titulo, this.status);
+                "Subtarefa - %s, %s", this.titulo, this.status ? "Concluida" : "Não concluida");
     }
 }
